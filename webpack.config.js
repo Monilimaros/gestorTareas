f172c3ee92288b6ce2899012224561bc890b5da6
Mónica Lima <monylimaros14214@gmail.com>
2024-08-05 13:24:21 -0600

    Config de webpack y estructura HTML

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js', //Punto de entrada de la aplicación
+    output: {
+        filename: 'bundle.js', //Nombre del archivo de salida
+        path: path.resolve(__dirname, 'dist'), //carpeta de salida
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/, //Regex para identificar archivos css
+                use: ['style-loader', 'css-loader'], //Loaders para procesar archivos css
+            },
+            {
+                test: /\.jss$/, //Regex para identificare archivos js
+                exclude: /node_modules/, // Excluir la carpeta nodule_modules
+                use: {
+                    loader: 'babel-loader', // Loader para llevar js moderno a js antiguo para que sea compatible con todos los navegadores
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    devtool: 'source-map', //Generar source maps para facilitar la depuración
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'), //Carpeta desde el cual el servidor agarrará los archivos
+        compress: true, //Habilitar la compresión qzip
+        part: 9000, // Puerto del servidor de desarrollo
+    },
+};
