f3d3f96f3c4e5323db30950d9b6371b36568233e
Mónica Lima <monylimaros14214@gmail.com>
2024-08-15 15:00:15 -0600

    Estilos CSS, función agregar tareas y función para visualizar tareas, UI.js

---
+++ src/task.js
@@ -0,0 +1,33 @@
+//lista de tareas
+let tasks = JSON.parse(localStorage.getItem("tasks")) || []; //almacenamiento Local en nuestro navegador/
+//función para llevar las tareas
+export const getTasks = () => tasks;
+//función para agregar una tarea
+export const addTask = (task) => {
+    const newtask = {
+        id: Date.now(),
+        text: task,
+        completed:false,
+    };
+    tasks.push(newtask);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+
+
+//Función para eliminar una tarea
+export const deleteTask = (id) => {
+    tasks = tasks,filter((task) => task.id !== parseInt (id));
+    localStorage.setItem("tasks", JSON,stringify(tasks));
+};
+
+//Función para actualizar una tarea
+export const toggleTask = (id) => {
+    task = tasks.map((task) => {
+        if(task.id === parseInt(id)) {
+            task.completed = !task.completed;
+        }
+        return task;
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
